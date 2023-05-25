e28fb33f0f1419b8805a32a59c4c674b8ae9faaf
ghouse8234 <134362145+ghouse8234@users.noreply.github.com>
2023-05-24 22:44:22 -0500

    main.py
    #boolean comparisions
    #adding numbers to strings

---
+++ main.py
@@ -0,0 +1,42 @@
+#local var and global var
+var = 101
+print(var)
+
+var = "i am learning python"
+print(var)
+print(var)
+
+
+
+
+
+
+
+
+
+
+#adding number to string
+age = 25
+statement = "hello swaroop, is your age is " + str(age)
+print(statement)
+
+#int input function using f-formatting
+user_age = int(input("enter your age:"))
+print(f"what is your age {user_age}")
+
+#and/or function(boolean comparisions)
+age = 24
+result_1 = age >= 21 and age < 50
+result_2 = age >= 21 or age < 50
+print(result_1, result_2)
+
+#and (If the value on the left of the `and` operator is truthy, we get the value on the right of the operator)
+x = True
+user_input = x and 10
+print(user_input)
+
+#multipication using numbers
+x = 56
+z = 20
+print(int(x * z))
+
